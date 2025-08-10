691b84274d73ef843a44d6b4c1154e82323a1d19
abdurrashidkhan <abdurrashidkhan.personal@gmail.com>
2025-08-10 09:32:27 +0600

    delete reducer create

--- client/src/redux/task/task.js
+++ client/src/redux/task/task.js
@@ -10,12 +10,17 @@ const tasks = createSlice({
     addTask: (state, { payload }) => {
       if (state.tasks.length === 0) {
         state.tasks.push({ id: 1, ...payload })
-      }else{
+      } else {
         const generateId = state.tasks.at(-1);
         state.tasks.push({ id: generateId.id + 1, ...payload })
       }
+    },
+    removeTask: (state, { payload }) => {
+      state.tasks.filter(item => {
+        item.id !== payload
+      })
     }
-  }
+  },
 })
 export const { addTask } = tasks.actions
 export default tasks.reducer;
